d8938add0b258e926ffd5afc5fbc79617b031797
kalpana-bandara <68723565+kalpana-bandara@users.noreply.github.com>
2023-12-22 06:45:16 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,6 @@
+// Create web server
+
+const commentController = require('../controllers/comment
+// Handle request
+router.get('/', comment Controller.comment_list);
+router.get('/create', commentController.comment_create_ge router.post('/create', commentController.comment_create_p router.get('/:id/delete', commentController.comment_delete
